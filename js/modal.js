b47d762933ff71e9366a1545ea18a0e7745e1b64
diamant1991 <diamant1988@mail.ru>
2015-03-06 14:19:59 +0200

    styles

--- js/modal.js
+++ js/modal.js
@@ -34,5 +34,52 @@ function opcl(arr, e){
         $('.popup-box-main').fadeOut(400)
         $('.popup-box-main').attr({'visible':'false'})
 	})
+ $('.quick-link').click(function(){
+      if($('.popup-tovar').attr('visible')!='true'){
+        $('.quick-form-mask').fadeIn(400);
+          $('.popup-tovar').fadeIn(400);
+          $('.popup-tovar').attr({'visible':'true'})
+      }
+      else{
+        $('.quick-form-mask').fadeOut(400);
+          $('.popup-tovar').fadeOut(400);
+          $('.popup-tovar').attr({'visible':'false'})
+      }
+  })
+ $('.btn-close, .quick-form-mask').click(function(){
+      $('.quick-form-mask').fadeOut(400)
+        $('.popup-tovar').fadeOut(400)
+        $('.popup-tovar').attr({'visible':'false'})
+  })
 
 });
+  function popup_position(){
+         var my_popup = $('.popup-tovar'), // наш попап
+                 my_popup_w = my_popup.width(), // ширина попапа
+                 my_popup_h = my_popup.height(), // высота попапа
+                 popup_half_w = my_popup_w/2, // половина ширины попапа
+                 popup_half_h = my_popup_h/2, // половина высоты попапа
+                 win_w = $(window).width(), // ширина окна
+                 win_h = $(window).height(); // высота окна
+ 
+         if ( win_w > my_popup_w ) { // если ширина окна больше ширины попапа
+                 my_popup.css({'margin-left':-popup_half_w, 'left': '50%'});
+         }
+         if ( win_w < my_popup_w ) { // если ширина окна меньше ширины попапа                  
+                 my_popup.css({'margin-left': 5, 'left': '0'});
+         }
+         if ( win_h > my_popup_h ) { // если высота окна больше ширины попапа
+                 my_popup.css({'margin-top': -popup_half_h, 'top':'45%'});
+         }
+         if ( win_h < my_popup_h ) { // если высота окна меньше ширины попапа
+                 my_popup.css({'margin-top': 5, 'top': '0'});
+         }
+ }
+
+        $(document).ready(function(){ 
+    popup_position();
+    });
+    $(window).resize(function(){  
+    popup_position();
+    });
+     
